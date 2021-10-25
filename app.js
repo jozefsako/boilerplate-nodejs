f49ae303d90a940e9873f1cdba0181bddc6fd111
Jozef Sako <jozef.sako@gmail.com>
2021-10-25 22:42:39 +0200

    feat: add user routes

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
 const app = express();
-const mongoose = require('mongoose');
-require('dotenv/config');
+const port = process.env.PORT || 3010;
 
 // Middlewares
+app.use(function (req, res, next) {
+  res.header("Content-Type", "application/json");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // Import Routes
-const usersController = require('./controllers/UsersController');
-app.use('/users', usersController);
+const usersController = require("./controllers/UsersController");
+app.use("/users", usersController);
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome the nodejs-boilerplate');
+app.get("/", (req, res) => {
+  res.send("Welcome the nodejs-boilerplate");
 });
 
-// Connect to DB 
+// Connect to DB
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
-    console.log('[DB]: connected');
+  console.log("[DB]: connected");
 });
 
-console.log(`[App]: is listening on the port: ${process.env.PORT}`);
-app.listen(process.env.PORT);
+console.log(`[App]: is listening on the port: ${port}`);
+app.listen(port);
--- models/User.js
+++ models/User.js
@@ -1,36 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-    firstname: {
-        type: String,
-        required: false,
-    },
-    lastname: {
-        type: String,
-        required: false,
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    phone: {
-        type: String,
-        required: false,
-    },
-    password: {
-        type: String,
-        required: false,
-    },
-    created: {
-        type: Date,
-        default: Date.now
-    },
+  firstname: {
+    type: String,
+    required: false,
+  },
+  lastname: {
+    type: String,
+    required: false,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: false,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Users', UserSchema);
+module.exports = mongoose.model("Users", UserSchema);
